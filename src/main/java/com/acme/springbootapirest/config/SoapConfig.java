506f37c4e2e24bd4cbb6e3e3ee6aff3b64cdf315
Nicodav28 <esteveznicolas0@gmail.com>
2025-03-02 00:53:41 -0500

    $

--- src/main/java/com/acme/springbootapirest/config/SoapConfig.java
+++ src/main/java/com/acme/springbootapirest/config/SoapConfig.java
@@ -4,6 +4,7 @@ import com.acme.springbootapirest.model.OrderRequestXML;
 import com.acme.springbootapirest.model.OrderResponseAcme;
 import com.acme.springbootapirest.model.OrderResponseXML;
 import com.acme.springbootapirest.model.OrdersXML;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.oxm.jaxb.Jaxb2Marshaller;
@@ -11,6 +12,10 @@ import org.springframework.ws.client.core.WebServiceTemplate;
 
 @Configuration
 public class SoapConfig {
+
+    @Value("${soap.service.url}")
+    private String soapServiceUrl;
+
     @Bean
     public Jaxb2Marshaller marshaller() {
         Jaxb2Marshaller marshaller = new Jaxb2Marshaller();
@@ -28,7 +33,7 @@ public class SoapConfig {
         WebServiceTemplate webServiceTemplate = new WebServiceTemplate();
         webServiceTemplate.setMarshaller(marshaller);
         webServiceTemplate.setUnmarshaller(marshaller);
-        webServiceTemplate.setDefaultUri("https://run.mocky.io/v3/ae7d2dc6-6924-4b1f-a485-bdec2081740b");
+        webServiceTemplate.setDefaultUri(soapServiceUrl);
         return webServiceTemplate;
     }
 }
